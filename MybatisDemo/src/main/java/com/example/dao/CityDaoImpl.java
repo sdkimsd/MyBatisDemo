97b0a2642a512a66b32952938ed1db3ca5fffd00
sdkimsd <tdkimtd2@naver.com>
2022-07-21 09:31:06 +0900

    MyBatis

--- MybatisDemo/src/main/java/com/example/dao/CityDaoImpl.java
+++ MybatisDemo/src/main/java/com/example/dao/CityDaoImpl.java
@@ -10,9 +10,6 @@ import com.example.vo.CityVo;
 
 @Repository("cityDao")
 public class CityDaoImpl implements CityDao {
-	
-	@Autowired
-	private SqlSession sqlSession;
 
 	@Override
 	public CityVo read(String name) {
